3a2e2077840515fd279bf22584581887a4e783dc
kai20060719 <kai20060719@gmail.com>
2025-05-18 20:41:11 +0900

    회원가입 구현

--- src/register.php
+++ src/register.php
@@ -5,7 +5,7 @@
 <form action = "test.php" method="post">
 <input type="text" name="id" placeholder="id를 입력하세요">
 <input type="text" name="password" placeholder="password를 입력하세요">
-<input type="submit" value="회원가입입">
+<input type="submit" value="회원가입">
 </form>
 </body>
 </html>
--- src/test.php
+++ src/test.php
@@ -1,3 +1,34 @@
 <?php
-echo $_POST["id"].$_POST["password"];
+$con = mysqli_connect("db","exampleuser","examplepass","exampledb");
+$set = "CREATE TABLE IF NOT EXISTS information(
+id VARCHAR(50) NOT NULL PRIMARY KEY,
+pw VARCHAR(50) NOT NULL 
+)";
+$ins = "INSERT INTO information(
+id,
+pw
+) VALUES(
+'{$_POST['id']}',
+'{$_POST['password']}'
+)";
+mysqli_query($con,$set);
+$sel = "SELECT * FROM information";
+$result = mysqli_query($con,$sel);
 ?>
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+while($row = mysqli_fetch_array($result)){
+    if(strcmp($row ['id'],$_POST['id']) == 0){
+        echo "이미 있는 id입니다.다시 입력해주세요<br>";
+        echo '<a href="http://localhost:8080/">돌아가기</a>';
+        return;
+    }
+}
+mysqli_query($con,$ins);
+echo "회원 가입 성공";
+?>
+<a href="http://localhost:8080/">돌아가기</a>
+</body>
+</html>
