9fd95cdc192912da74b422ee4d86e27308ba4315
kai20060719 <kai20060719@gmail.com>
2025-05-19 17:50:06 +0900

    글수정 & 글 삭제 기능이 글쓴이에게 보이도록

--- src/viewtextexe.php
+++ src/viewtextexe.php
@@ -11,6 +11,12 @@ $row = mysqli_fetch_assoc($result);
 <h1> <?php echo $row['title'] ?> <br> </h1>
 <?php echo '<p>작성자: '.$row['user_id'].'</p>'; ?>
 <?php echo '<p>작성시간: '.$row['created'].'</p>'; ?>
+<?php 
+if (strcmp($row['user_id'],$_SESSION['user_id']) == 0){
+echo '<p><a href="http://localhost:8080/textupdate.php?id='.$row['id'].'">글 수정</a>    ';  
+echo '<a href="http://localhost:8080/textdelete.php?id='.$row['id'].'">글 삭제</a></p>';
+}
+?>
 <?php echo '<p>'.$row['description'].'</p>'; ?>
 <p><a href="http://localhost:8080/member.php">돌아가기</a></p>
 </form>
